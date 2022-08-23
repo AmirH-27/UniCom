48311d5c00116e8c82a0ca82d6f4d67297e814ef
jubayer <jubayertalha194@gmail.com>
2022-08-23 23:33:40 +0600

    add websocket

--- src/main/java/com/config/WebSocketConfig.java
+++ src/main/java/com/config/WebSocketConfig.java
@@ -1,56 +1,34 @@
 package com.config;
 
 
-
-////@Configuration
-////@EnableWebSocketMessageBroker
-////@EnableWebSocket
-////@EnableWebMvc
-//public class WebSocketConfig {
-//
-////    @Override
-////    public void configureMessageBroker(MessageBrokerRegistry registry) {
-////        registry.enableSimpleBroker("/topic");
-////        registry.setApplicationDestinationPrefixes("/app");
-////    }
-////
-////    @Override
-////    public void registerStompEndpoints(StompEndpointRegistry registry) {
-//////        RequestUpgradeStrategy upgradeStrategy = new TomcatRequestUpgradeStrategy();
-//////        registry.addEndpoint("/ws")
-//////                .withSockJS();
-//////
-//////        registry.addEndpoint("/ws")
-//////                .setHandshakeHandler(new DefaultHandshakeHandler(upgradeStrategy))
-//////                .setAllowedOrigins("*");
-////
-////    }
-////    @Override
-////    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-////        registry.addHandler(new MyHandler(), "/myHandler")
-////            .setAllowedOrigins("*");
-////    }
-//}
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.server.RequestUpgradeStrategy;
+import org.springframework.web.socket.server.standard.TomcatRequestUpgradeStrategy;
+import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
 
 @Configuration
 @EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
 
     @Override
-    public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic");
-        config.setApplicationDestinationPrefixes("/app");
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topic");
+        registry.setApplicationDestinationPrefixes("/app");
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/gs-guide-websocket").withSockJS();
-    }
+        RequestUpgradeStrategy upgradeStrategy = new TomcatRequestUpgradeStrategy();
+        registry.addEndpoint("/ws")
+                .withSockJS();
 
+        registry.addEndpoint("/ws")
+                .setHandshakeHandler(new DefaultHandshakeHandler(upgradeStrategy))
+                .setAllowedOrigins("*");
+
+    }
 }
--- src/main/java/com/controller/ChannelController.java
+++ src/main/java/com/controller/ChannelController.java
@@ -1,14 +1,13 @@
 package com.controller;
 
-import com.model.Channel;
-import com.model.MemberChannel;
-import com.model.Message;
-import com.model.User;
+import com.model.*;
 import com.service.ChannelService;
 import com.service.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.propertyeditors.StringTrimmerEditor;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.WebDataBinder;
@@ -39,19 +38,19 @@ public class ChannelController {
         List<Message> messages = messageService.getAll(channel.getChannelId());
         model.addAttribute("members", channel.getChannelMembers());
         model.addAttribute("messages", messages);
+        String path = request.getServletPath();
+        model.addAttribute("path", path);
         return "channel";
     }
 
-//    @MessageMapping("/ws")
-//    @SendTo("/topic/messages")
-//    public Message response(String message){
-//        try {
-//            Thread.sleep(1000);
-//        } catch (InterruptedException e) {
-//            throw new RuntimeException(e);
-//        }
-//        Message response = new Message();
-//        response.setMessage(message);
-//        return response;
-//    }
+    @MessageMapping("/ws")
+    @SendTo("/topic/messages")
+    public HelloMessage response(String message){
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        return response(message);
+    }
 }
